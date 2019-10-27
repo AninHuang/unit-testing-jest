dab2b8a094a7785771dc1b87b626c758f65925f9
AninHuang <lifeanne19904@gmail.com>
2019-10-27 11:39:54 +0800

    Test null, falsy and object

--- functions.js
+++ functions.js
@@ -3,6 +3,14 @@ const functions = {
   // add: function(num1, num2) {
   //   return num1 + num2;
   // }
+  isNull: () => null,
+  checkValue: x => x,
+  createUser: () => {
+    const user = { firstName: 'Anin' };
+
+    user['lastName'] = 'Huang';
+    return user;
+  }
 }
 
 module.exports = functions;
